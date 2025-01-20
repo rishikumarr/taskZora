72be01fc2fb42cce8e4475d4d62c146dc9e9e787
rishikumarr <rishikumaarrbtech@gmail.com>
2025-01-20 15:04:40 +0530

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { createBrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <h1 className='bg-slate-500'>TODO app</h1>
+      <button>Submit</button>
+    </>
+  )
+}
+
+export default App
